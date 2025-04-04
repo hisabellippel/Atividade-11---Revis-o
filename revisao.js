d2f0e7b6963fa14633d25857c1a63c9c0c5df974
Hisabel <hisabel_lippel@estudante.sesisenai.org.br>
2025-04-04 09:33:03 -0300

    add questão 2

--- revisao.js
+++ revisao.js
@@ -1,4 +1,4 @@
-//questão 1
+/*questão 1
 let n1 = 0
 let n3 = 0
 let soma = 0
@@ -10,4 +10,24 @@ do{
     console.log(soma + n3)
     n1 = n1 + 1
 }
-while(somar > n1);
+while(somar > n1);*/
+
+//questão 2
+let intervalo = null;
+
+var timer = function(){
+    let count = 10;
+    intervalo = setInterval(() =>{
+        console.log(count);
+        count--
+        }, 1000)}
+
+var stop = function(){
+    setTimeout(() => {
+        clearInterval(intervalo)
+        console.log('Lançamento');        
+            }, 10000)
+    }
+
+timer();
+stop();
