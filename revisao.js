2011aa5df233c00e1f73c789f3d1574fdcf6c5bd
Hisabel <hisabel_lippel@estudante.sesisenai.org.br>
2025-04-04 08:58:25 -0300

    add questão 1

---
+++ revisao.js
@@ -0,0 +1,13 @@
+//questão 1
+let n1 = 0
+let n3 = 0
+let soma = 0
+let somar = parseFloat(prompt('Digite quantos números você quer somar:'));
+
+do{
+    let n2 = parseFloat(prompt('digite seu número '));
+    soma = soma + n3
+    console.log(soma + n3)
+    n1 = n1 + 1
+}
+while(somar > n1);
